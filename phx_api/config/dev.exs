30d25e7fa0760112a26627eee91054cdc04f77df
simlf <simon.le-fourn@epitech.eu>
2023-10-25 16:32:25 +0200

    🔧 Update HTTP Endpoint so that it can be accesible outside Docker

--- phx_api/config/dev.exs
+++ phx_api/config/dev.exs
@@ -2,10 +2,15 @@ import Config
 
 # Configure your database
 config :phx_api, TimeManager.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "phx_api_dev",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  hostname: System.get_env("PGHOST"),
+  database: System.get_env("PGDATABASE"),
+  port: System.get_env("PGPORT"),
+  # username: "postgres",
+  # password: "postgres",
+  # hostname: "localhost",
+  # database: "phx_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +24,7 @@ config :phx_api, TimeManager.Repo,
 config :phx_api, TimeManagerWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
