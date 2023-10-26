c1748b1de7c1f4a23619c005a0b31f3310cf3338
simlf <simon.le-fourn@epitech.eu>
2023-10-26 16:32:55 +0200

    🔥 Remove code from Vue project initialization

--- web_app/src/router/index.ts
+++ web_app/src/router/index.ts
@@ -1,27 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/chartManager',
       name: 'chartManager',
       component: () => import('../components/ChartManager.vue')
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../components/user/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../components/user/Register.vue')
+    },
   ]
 })
 
