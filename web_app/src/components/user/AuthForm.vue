1c452d049f42a4a803604c8955441d8e04799805
Enzo <enzo.stephan@epitech.eu>
2023-11-04 23:12:01 +0100

    minor front upgrades

--- web_app/src/components/user/AuthForm.vue
+++ web_app/src/components/user/AuthForm.vue
@@ -41,22 +41,12 @@
         </div>
 
         <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
-              >Password</label
-            >
-          </div>
-          <div class="mt-2">
-            <input
-              v-model="formInfo.password"
-              name="password"
-              type="password"
-              autocomplete="current-password"
-              placeholder="Password"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
+            <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            </div>
+            <div class="mt-2">
+                <input v-model="formInfo.password" minlength=6 name="password" type="password" autocomplete="current-password" placeholder="Password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
         </div>
 
         <div>
--- web_app/src/components/user/Register.vue
+++ web_app/src/components/user/Register.vue
@@ -14,19 +14,22 @@ const createUser = (data: { username: string; email: string; password: string })
 </script>
 
 <template>
-  <div v-if="!authStore.isAuthenticated">
-    <div>
-      <AlertBox v-if="authStore.error" type="error" :message="errorMessage" @dismiss="clearError" />
-      <AlertBox
-        v-if="authStore.success"
-        type="success"
-        :message="successMessage"
-        @dismiss="clearSuccess"
-      />
-      <AuthForm formTitle="Register" submitLabel="Register" @form-submit="createUser" />
+  <div class="flex flex-col items-center mt-2">
+    <div class=" w-full sm:w-1/8 md:w-1/8 lg:w-1/8 xl:w-1/8">
+      <div v-if="!authStore.isAuthenticated">
+        <AlertBox v-if="authStore.error" type="error" :message="errorMessage" @dismiss="clearError" />
+        <AlertBox
+          v-if="authStore.success"
+          type="success"
+          :message="successMessage"
+          @dismiss="clearSuccess"
+        />
+        <AuthForm formTitle="Register" submitLabel="Register" @form-submit="createUser" />
+        <div class="text-center">
+          <RouterLink to="/login" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go Back</RouterLink>
+        </div>
+      </div>
     </div>
-    <div class="mt-1">
-      <RouterLink to="/login" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go Back</RouterLink>
-    </div>
+
   </div>
 </template>
