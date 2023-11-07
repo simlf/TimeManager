5fdfd707cef98e98a3045ce8ecdb39d83d360259
simlf <simon.le-fourn@epitech.eu>
2023-11-07 20:44:40 +0100

    ✅ Add E2E tests

--- web_app/cypress.config.ts
+++ web_app/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: '6dr2uu',
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173'
--- web_app/cypress/e2e/main-flow.cy.ts
+++ web_app/cypress/e2e/main-flow.cy.ts
@@ -1,38 +1,50 @@
-// cypress/integration/main-flow.spec.js
-
 describe('Main Flow', () => {
+  beforeEach(() => {
+    // Load user data and log in before each test
+    cy.fixture('users/employee.json').then((userData) => {
+      cy.login(userData.email, userData.password)
+    })
+  })
+
+  it('should click on the Start button of the Clock Manager', () => {
+    cy.fixture('users/employee.json').then((userData) => {
+      cy.visit(`/clockManager/${userData.username.replace(/ /g, '%20')}`)
+      cy.contains('Start').should('be.visible').wait(1000).click()
+    })
+  })
+
+  it('should click on the Break time button of the Clock Manager', () => {
+    cy.fixture('users/employee.json').then((userData) => {
+      cy.visit(`/clockManager/${userData.username.replace(/ /g, '%20')}`)
+      cy.contains('Break Time').should('be.visible').wait(1000).click()
+      cy.wait(1000)
+    })
+  })
+
+  it('should click on the Stop button of the Clock Manager', () => {
+    cy.fixture('users/employee.json').then((userData) => {
+      cy.visit(`/clockManager/${userData.username.replace(/ /g, '%20')}`)
+      cy.contains('Stop').should('be.visible').wait(1000).click()
+    })
+  })
+
   it('should navigate to the home page', () => {
-    cy.visit('/');
-    // Add assertions for the home page content, if needed
-  });
+    // Wait for the navigation to the home page to be complete
+    cy.url().should('eq', Cypress.config().baseUrl + '/')
+  })
 
-  it('should register a new user', () => {
-    cy.visit('/register');
-    // Fill in registration form fields
-    cy.get('input[name="username"]').type('newuser');
-    cy.get('input[name="email"]').type('newuser@example.com');
-    cy.get('input[name="password"]').type('newpassword');
-    // Click the registration button
-    cy.get('button[type="submit"]').click();
-    // Add assertions for successful registration or error messages
-  });
+  it('should not have the Login button on the NavBar when logged in', () => {
+    cy.get('nav').should('not.contain', 'Login')
+  })
 
-  it('should log in with registered user', () => {
-    cy.visit('/login');
-    // Fill in login form fields with the credentials of the registered user
-    cy.get('input[name="email"]').type('newuser@example.com');
-    cy.get('input[name="password"]').type('newpassword');
-    // Click the login button
-    cy.get('button[type="submit"]').click();
-    // Add assertions for successful login or error messages
-  });
+  it('should click on the Clock Manager button on the NavBar', () => {
+    cy.get('nav').contains('Clock Manager').should('be.visible').wait(2000).click()
 
-  it('should start a Clock Manager', () => {
-    // You may need to perform some actions to reach the Clock Manager page
-    // For example, you might need to navigate through your app's navigation menu
-    // or perform specific interactions.
-    // Once on the Clock Manager page, add assertions to verify that it's loaded correctly.
-    cy.visit('/clockManager');
-    // Add assertions for the Clock Manager page content
-  });
-});
+    cy.fixture('users/employee.json').then((userData) => {
+      const expectedUrl = `/clockManager/${userData.username.replace(/ /g, '%20')}`
+      cy.log('Expected URL:', expectedUrl)
+
+      cy.url().should('include', expectedUrl)
+    })
+  })
+})
