7ea22d673bb494d8e82b3a474ca3ab208ecec8e4
Maxence Nowak <maxence.nowak@gmail.com>
2023-11-09 14:23:32 +0100

    Clean dockerfile, Fix docker-compos

--- phx_api/entrypoint.sh
+++ phx_api/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Docker entrypoint script.
 
+mix local.hex --force && \
+    mix local.rebar --force && \
+    mix archive.install --force hex phx_new 1.5.1
+
 # Wait until Postgres is ready
 echo "Testing if Postgres is accepting connections. {$PGHOST} {$PGPORT} ${PGUSER}"
 while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
@@ -9,12 +13,12 @@ do
   sleep 2
 done
 
-cd phx_api
+# cd phx_api
+  mix deps.get
 
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE does not exist. Creating..."
-  mix deps.get
   mix ecto.create
   mix ecto.migrate
   mix run priv/repo/seeds.exs
