bd9baec9f94de423b2888a4ec6bd8369fa2cc333
Maxence Nowak <maxence.nowak@gmail.com>
2023-11-11 23:56:48 +0100

    Create clock on creation of user. Change field required for update and create clock. Fix some problem on workingtimes. Adding new version of postman

--- phx_api/priv/repo/migrations/3_create_clocks.exs
+++ phx_api/priv/repo/migrations/3_create_clocks.exs
@@ -3,11 +3,12 @@ defmodule TimeManager.Repo.Migrations.CreateClocks do
 
   def change do
     create table(:clocks) do
-      add :time, :naive_datetime, null: false
-      add :status, :boolean, default: false, null: false
-      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add(:time, :naive_datetime, null: true)
+      add(:status, :boolean, default: false, null: false)
+      add(:user_id, references(:users, on_delete: :delete_all), null: false)
       timestamps(type: :utc_datetime)
     end
-    create unique_index(:clocks, [:user_id])
+
+    create(unique_index(:clocks, [:user_id]))
   end
 end
